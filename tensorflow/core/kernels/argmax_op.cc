92a1d205150583b0e596432ff2a0f9737bb5675a
luke iwanski <luke@codeplay.com>
2017-07-13 17:35:12 +0100

    [OpenCL] Extends ArgMax to cover integral types

--- tensorflow/core/kernels/argmax_op.cc
+++ tensorflow/core/kernels/argmax_op.cc
@@ -259,7 +259,10 @@ REGISTER_KERNEL_BUILDER(Name("ArgMin")                              \
                             .HostMemory("dimension"),               \
                         ArgMinOp<SYCLDevice, type, int32>);
 
-TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_ARGMAX_SYCL);
+//TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_ARGMAX_SYCL);
+TF_CALL_INTEGRAL_TYPES(REGISTER_ARGMAX_SYCL);
+REGISTER_ARGMAX_SYCL(float);
+REGISTER_ARGMAX_SYCL(double);
 #undef REGISTER_ARGMAX_SYCL
 #endif  // TENSORFLOW_USE_SYCL
 }  // namespace tensorflow
