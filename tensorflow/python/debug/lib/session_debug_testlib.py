d83ca495079d869143d083781b430549eaad4d3f
John Lawson <john@codeplay.com>
2017-07-25 17:07:51 +0100

    [OpenCL] Fixes device name comparison in testlib (#150)
    The device name used in the tests should not be assumed to be "/gpu:0",
    but should use the value provided by `gpu_device_name()`.

--- tensorflow/python/debug/lib/session_debug_testlib.py
+++ tensorflow/python/debug/lib/session_debug_testlib.py
@@ -709,9 +709,10 @@ class SessionDebugTestBase(test_util.TensorFlowTestCase):
     u_read_name = u_name + "/read"
 
     # Test node name list lookup of the DebugDumpDir object.
-    if test_util.gpu_device_name():
-      node_names = dump.nodes(
-          device_name="/job:localhost/replica:0/task:0/gpu:0")
+    dev_name = test_util.gpu_device_name()
+    if dev_name:
+      dev_name = "/job:localhost/replica:0/task:0" + dev_name
+      node_names = dump.nodes(device_name=dev_name)
     else:
       node_names = dump.nodes()
     self.assertTrue(u_name in node_names)
