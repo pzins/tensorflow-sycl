19addde94c5abedf7e3e392b402d1bcbf6c06e74
Luke Iwanski <lukasz.iwanski@gmail.com>
2018-02-06 12:53:44 +0100

    Fixes automerge

--- tensorflow/core/framework/register_types.h
+++ tensorflow/core/framework/register_types.h
@@ -222,8 +222,8 @@ limitations under the License.
 #ifdef __ANDROID_TYPES_SLIM__
 #define TF_CALL_SYCL_NUMBER_TYPES(m) TF_CALL_float(m)
 #else  // __ANDROID_TYPES_SLIM__
-#define TF_CALL_SYCL_NUMBER_TYPES(m) TF_CALL_float(m) TF_CALL_SYCL_double(m)
-    TF_CALL_SYCL_half(m)                \
+#define TF_CALL_SYCL_NUMBER_TYPES(m) TF_CALL_float(m) TF_CALL_SYCL_double(m) \
+  TF_CALL_SYCL_half(m)
 #endif  // __ANDROID_TYPES_SLIM__
 
 #endif  // TENSORFLOW_FRAMEWORK_REGISTER_TYPES_H_
